53e2ba004d02b91617388389e41c07ad30d4a858
lucas <lucas.baldassarre@protonmail.com>
2022-03-31 18:45:23 +0200

    +silent +coms

--- GenePass.py
+++ GenePass.py
@@ -1,71 +1,101 @@
-import random
 import sys
+import random
 import getopt
-
+import pyperclip
+"""from timeit import default_timer as timer"""
+global info, silent
 # Options
-options = "hn:l:o:"
+options = "hn:l:o:s"
 # Long options
-long_options = ["help", "number =", "length =", "output ="]
-# list of the characters avalable
-charlist = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+long_options = ["help", "number =", "length =", "output =", "silent"]
+
+# list of the characters available
+numbers = "0123456789"
+lettres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+symboles = "!\"#$%&'()*+,-./:<=>?@[\]^_`{|}~;"
+charlist = numbers + lettres + symboles
 
 
 def infos():
+    global info
+    info = True
+    print("GenePass.py\n"
+          "OPTION\n"
+          "-h   --help      <Show this page>\n"
+          "-n   --number    <Number of Passwords>   (by default = 1)\n"
+          "-l   --length    <Password Length>       (by default = 128)\n"
+          "-o   --output    <output-file>\n"
+          "-s   --silent    <don't show any passwords>\n"
+          "\n"
+          "if you generate only one password its while be copied to your clipboard\n"
+          "\n"
+          "EXAMPLES:\n"
+          "PassGene.py -n 10 -l 50 -o password_list.txt\n"
+          "PassGene.py -h\n"
+          "\n"
+          "SEE THE MAN PAGE https://github.com/M0ShYy/Password_Generator FOR MORE OPTIONS AND EXAMPLES\n")
 
-   print("GenePass.py\nOPTION\n-h	--help		<Show this page>\n-n	--number    <Number of Passwords>\n-l	--length  	<Password Length>\n-o	--output  	<outputfile>\n\nEXAMPLES:\nPassGene.py -n 10 -l 50 -o pass.txt\nPassGene.py -h\nSEE THE MAN PAGE (https://github.com/M0ShYy/Password_Generator) FOR MORE OPTIONS AND EXAMPLES\n")
 
-
-
-def creapassword(nombre,longueur,output):
-    #outfile =''
-    nombre = int(nombre)
+def creapassword(nombre, longueur, output):
+    global silent, charlist
+    nombre = int(nombre)                                                    # make sure the variables are int
     longueur = int(longueur)
     password = ""
-    for i in range(0, nombre):  # pour chaque mot de passe
-        for j in range(0, longueur):  # pour chaque caractere du mot de pass
-            charnb = (random.randrange(len(charlist)))  # prend la position d'un character dans la liste
-            password = password + charlist[charnb]  # metre les characters les uns a la suite des autres
-        #print('\033[1;31mmot de pass N°' + str(int(i) + 1) + ' :\033[1;m\n' + password)  # affichage du mot de pass colorer
-        print('mot de pass N°' + str(int(i) + 1) + ' :\n' + password)  # affichage du mot de pass non colorer
-        if output:
-            outfile= open(output, "a")
-            outfile.write(password + "\n\n")
 
-        password = ""  # remetre a zero le mots de passe
-    #if output:
-        #outfile.close()
+    for i in range(0, nombre):                                              # foreach password(s)
+        for j in range(0, longueur):                                        # foreach characters
+            charnb = (random.randrange(len(charlist)))                      # take a random character in the list
+            password = password + charlist[charnb]                          # append the characters
+        if not silent:                                                      # if the silent option as not being used
+            print('mot de pass N°' + str(int(i) + 1) + ' :\n' + password)   # print the password(s)
+        if output:                                                          # if the output option as being used
+            outfile = open(output, "a")                                     # create/open the file in append mode
+            outfile.write(password + '\n')                                  # print the password in and do a line break
+        if nombre == 1:                                                     # if there is only one password
+            pyperclip.copy(password)                                        # copy it to the clipboard
+        password = ""                                                       # clear the password variable
 
 
 def main(argumentlist):
-    nombre = int(0)
-    longueur = int(0)
+    global info, silent
+    info = False
+    silent = False
+    nombre = int(1)         # default number of password
+    longueur = int(128)     # default number of character in the password(s)
     outputfile = ''
-    if not argumentlist:
-        infos()
+
     try:
         arguments, values = getopt.getopt(argumentlist, options, long_options)
 
         for currentArgument, currentValue in arguments:
 
-            if currentArgument in ("-h", "--help"):     #help commande
-                infos()
-            elif currentArgument in ("-n","--number "):
-                nombre = currentValue
-
-            elif currentArgument in ("-l", "--length "):
-                longueur = currentValue
-            elif currentArgument in ("-o", "--output "):
-                outputfile = currentValue
-
-        creapassword(nombre,longueur,outputfile)
-    except getopt.error as err:
+            if currentArgument in ("-h", "--help"):         # test if there is -h or --help
+                infos()                                     # print help
+            elif currentArgument in ("-n", "--number "):    # test if there is -n or -_number
+                nombre = currentValue                       # take the value of the number of password(s)
+            elif currentArgument in ("-l", "--length "):    # test if there is -l or -- length
+                longueur = currentValue                     # take the value of the length of the password(s)
+            elif currentArgument in ("-o", "--output "):    # test if there is -o --output
+                outputfile = currentValue                   # take the value of the output-file
+            elif currentArgument in ("-s", "--silent "):    # test if there is -s or --silent
+                silent = True
+        if not info:                                        # if the help was not printed then
+            creapassword(nombre, longueur, outputfile)
+    except getopt.error as err:                             # if there is an error
         # output error, and return with an error code
-        print(str(err))
-        infos()
+        print(str(err))                                     # print it
+        infos()                                             # and print the help
         sys.exit()
 
-if __name__ == '__main__':
-    #argumentList = (" --help".split())
-    #print(argumentList)
-    argumentList = (sys.argv[1:])
+
+argumentList = (sys.argv[1:])                               # make a list of all the option wrote by the user
+main(argumentList)
+
+"""if __name__ == '__main__':
+    argument = "-s -l 10000 -n 10000"
+    argumentList = (argument.split())
+    print(argumentList)
+    start = timer()
     main(argumentList)
+    print("Password generated in:", timer() - start, "seconds")
+"""
